5330ffbe4dda25ef0a045a06d55dc01a3344c49f
rladntlr <tlrrns08@gmail.com>
2025-12-09 11:23:52 +0900

    Initial commit

---
+++ src/test/java/springboot/giftledger/GiftLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.giftledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftLedgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
